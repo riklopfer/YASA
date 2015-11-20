aa641f2eb945da3b675386f9a6bc5ad1d0fef91b
rklopfer <russell@klopfer.us>
2015-11-20 13:38:24 -0500

    fixing setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='YASA',
-      version='0.0.2',
+      version='0.0.3',
       description='Sequence aligners written in python',
       author='Russell Klopfer',
       author_email='russell@klopfer.us',
       url='https://github.com/riklopfer/YASA',
-      packages=['yasa'],
+      packages=['yasa', 'yasa.aligners'],
       )
